c02ed12ef70d95f9ba642f99a407bb607b794eb6
ntkhanggggg <84844986+ntkhanggggg@users.noreply.github.com>
2021-07-29 19:54:46 +0700

    Update newgbanbyntkhang.js

--- newgbanbyntkhang.js
+++ newgbanbyntkhang.js
@@ -3,8 +3,12 @@
      "reason": "Leak code, bú fame của NTKhang",
      "date": "29/7"
   },
-  "100005303558824" :{
+  "100005303558824": {
      "reason": "Leak code",
+     "date": "29/7"
+  },
+  "100044981643083": {
+     "reason": "V",
      "date": "29/7"
   }
 }
